caeb98decf7b8aa141fad4dbbe4653c95092baef
mauricekaru <Maurice@shalomsoft.com>
2020-10-26 09:52:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
+<?php 
+$now = time();
+$secondsDelay = 86400; //one-day
+
+?>
 <html>
-<head></head>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="language" content="en" /> 
+  <meta http-equiv="expires" content="<?php echo date('D, d M Y H:i:s O \G\M\T', ($now-$secondsDelay)) ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+  <title>Maurice Test Page</title>
+</head>
 <body>
-<h1>Hello World!</h1>
+  <h1>Hello World!</h1>
 </body>
 </html>
